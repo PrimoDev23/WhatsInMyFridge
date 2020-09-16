80b5d3a18a4cc6dd6a3a3206b4d6eca76d8202e5
MarcusCoding <marcus_voigt@gmx.net>
2020-09-16 12:42:33 +0200

    recipe detail view [+] detail view for recipes [+] better ui for recipe overview

---
+++ WhatsInMyFridge/WhatsInMyFridge/Converter/FridgeStateToImage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WhatsInMyFridge.Converter
+{
+    public class FridgeStateToImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null)
+            {
+                return null;
+            }
+
+            if((bool)value == true)
+            {
+                return new SolidColorBrush(Color.Green);
+            }
+            else
+            {
+                return new SolidColorBrush(Color.IndianRed);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
